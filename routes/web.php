8dad3870865471a3eb3fea3dd49d03cd265c3129
WelberthMagalhaes <welberthheider@gmail.com>
2023-07-31 17:34:31 -0300

    menu navegacao

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,3 @@ Route::get('/', [PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [ContatoController::class, 'contato']);
-
-Route::get(
-    '/contato/{nome}/{categoria_id}',
-    function (
-        string $nome,
-        int $categoria_id = 1, //1 - 'Informação'
-    ) {
-        echo "Estamos aqui $nome - $categoria_id ";
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[a-zA-Z]+');
