a5510c6337571e914547bc29e3e2aa04622bf2d5
WelberthMagalhaes <welberthheider@gmail.com>
2023-12-05 15:43:55 -0300

    Blade - estrutura  @if/@else

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function index() {
-        return view('app.fornecedores.index');
+
+    public function index()
+    {
+
+        // $fornecedores = ['Fornecedor 1'];
+        $fornecedores = [1,2,3,4,5,6,7,8,9];
+
+        return view('app.fornecedores.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedores/index.blade.php
+++ resources/views/app/fornecedores/index.blade.php
@@ -1,9 +1,19 @@
 <h3>Fornecedor</h3>
 
-{{-- Comentario --}}
-{{'Texto de teste'}}
-<?= 'Texto de teste' ?>
-
 @php
-    // trecho php puro dentro do blade
+    // if(){
+
+    // }elseif (condition) {
+    //     # code...
+    // }else {
+    //     # code...
+    // }
 @endphp
+
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados.</h3>
+@elseif(count($fornecedores) >= 10)
+    <h3>Existem vários fornecedores cadastrados.</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados.</h3>
+@endif
